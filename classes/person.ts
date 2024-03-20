ed34b5ed14f78136b560f05344d2ea13ba58d651
miketorii <miketorii@gmail.com>
2024-03-20 13:20:11 +0900

    add class

--- classes/person.ts
+++ classes/person.ts
@@ -1,3 +1,23 @@
-let message: string = 'Hello, World!';
+class Person {
+      ssn: string;
+      firstName: string;
+      lastName: string;
 
+      constructor(ssn: string, firstName: string, lastName: string){
+      		       this.ssn = ssn;
+		       this.firstName = firstName;
+		       this.lastName = lastName;
+      }
+
+      getFullName(): string {
+      		     return this.firstName + ' ' + this.lastName;
+      }
+}
+
+let message: string = 'Hello, World!';
 console.log(message);
+
+let person = new Person('123456789', 'Mike', 'Torii');
+console.log( person.getFullName() );
+
+
