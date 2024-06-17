6dc86089e5db2774ac82a15891cbf2596624a86a
miketorii <miketorii@gmail.com>
2024-06-17 21:52:51 +0900

    add let

--- restparam/restparam.ts
+++ restparam/restparam.ts
@@ -7,3 +7,25 @@ function iTakeItAll(first, second, ...others){
 
 iTakeItAll("foo","bar");
 iTakeItAll("foo","bar","bas", "qux");
+
+let foo = 123;
+if(true){
+    let foo = 456;
+}
+console.log(foo);
+
+var funcs = [];
+for(var i = 0; i<3; i++){
+    funcs.push( function(){ console.log(i); } )
+}
+for(var j=0; j<3; j++){
+    funcs[j]();
+}
+
+let funcs2 = [];
+for(let i = 0; i<3; i++){
+    funcs2.push( function(){ console.log(i); } )
+}
+for(let j=0; j<3; j++){
+    funcs2[j]();
+}
