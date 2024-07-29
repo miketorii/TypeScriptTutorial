142e5f31c1463d2985bc2a56a8bb690eaddeb846
miketorii <miketorii@gmail.com>
2024-07-29 21:53:33 +0900

    add enum

--- types/types.ts
+++ types/types.ts
@@ -13,6 +13,26 @@ var card = CardSuit.Diamonds;
 console.log("----------")
 console.log(card);
 
+enum Color {
+    DarkRed = 3,
+    DarkGreen,
+    DarkBlue
+}
+
+console.log("enum Color: ", Color.DarkGreen);
+
+enum AnimalFlags {
+    None        = 0,
+    HasClows    = 1 << 0,
+    CanFly      = 1 << 1,
+    EatsFish    = 1 << 2,
+    Endangered  = 1 << 3
+}
+
+console.log("enum AnimalFlags.HasClows=", AnimalFlags.HasClows);
+console.log("enum AnimalFlags.EatsFish=", AnimalFlags.EatsFish);
+console.log("enum AnimalFlags.Endangered=", AnimalFlags.Endangered);
+
 //////////////////////////////////////////////////
 //  interface
 
