2884f2e2a5199d3f9325a37048fc30919eb01c07
miketorii <miketorii@gmail.com>
2024-07-30 21:38:04 +0900

    add type

---
+++ types/typeerr.ts
@@ -0,0 +1,20 @@
+type SomethingComplex = {
+    foo: number,
+    bar: string
+}
+
+function takeSomethingComplex(arg: SomethingComplex){
+    console.log(arg.foo);
+    console.log(arg.bar);
+}
+
+function getBar() : string {
+    return "some bar";
+}
+
+const fail = {
+    foo: 123,
+    bar: getBar()
+}
+
+takeSomethingComplex(fail);
