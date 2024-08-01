60793178106f3840e0652d3e5dafc9c8cb68a595
miketorii <miketorii@gmail.com>
2024-08-01 22:09:59 +0900

    add fill

--- types/createarray.ts
+++ types/createarray.ts
@@ -1,2 +1,13 @@
 const foo:string[] = new Array(3).fill('a');
 console.log(foo);
+
+class Foo<T>{
+    foo: T;
+}
+
+let FooNumber = Foo as { new ():Foo<number> };
+console.log(FooNumber);
+
+let aa = new Foo<number>;
+aa.foo = 3;
+console.log(aa.foo);
