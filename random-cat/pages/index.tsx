62c3043786aef12fdce44785b0705c4bac5c5497
miketorii <miketorii@gmail.com>
2024-08-03 10:42:48 +0900

    add nextjs

--- random-cat/pages/index.tsx
+++ random-cat/pages/index.tsx
@@ -1,10 +1,17 @@
-import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
 
-const IndexPage: NextPage = () => {
-    const [imageUrl, setImageUrl] = useState("");
+import styles from "./index.module.css";
+
+type Props = {
+    initialImageUrl: string;
+};
+
+const IndexPage: NextPage<Props> = ( {initialImageUrl} ) => {
+    const [imageUrl, setImageUrl] = useState(initialImageUrl);
     const [loading, setLoading] = useState(true);
 
+    /*
     useEffect( () => {
         fetchImage().then(
             (newImage) => {
@@ -12,11 +19,35 @@ const IndexPage: NextPage = () => {
                 setLoading(false);
             }
         );
-    }, []);
+    }, []); 
+    */
 
-    return <div>{loading || <img src={imageUrl} /> }</div>;
+    const handleClick = async () => {
+        setLoading(true);
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url);
+        setLoading(false);
+    };
+
+    return (
+        <div className={styles.page} >
+            <button onClick={handleClick} className={styles.button} >Show others</button>
+            <div className={styles.frame} >
+                {loading || <img src={imageUrl} /> }
+            </div>
+        </div>
+    );
 };
 export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const image = await fetchImage();
+    return {
+        props: {
+            initialImageUrl: image.url
+        }
+    };
+};
 
 type Image = {
     url: string;
