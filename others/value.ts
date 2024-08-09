72179a6e074da6a8d4ee8e606c07c5bc4f2ae99c
miketorii <miketorii@gmail.com>
2024-08-10 06:25:18 +0900

    add type any

--- others/value.ts
+++ others/value.ts
@@ -3,3 +3,12 @@ const strObj = new String(str);
 console.log(strObj.length);
 console.log(strObj.toUpperCase());
 
+//////////////////////////////////////////
+//
+let val : any;
+val = 1;
+console.log("val number: ", val);
+val = "mike";
+console.log("val string: ", val);
+val = { name: "object" };
+console.log("val object: ", val);
