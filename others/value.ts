6f905ed9cfd11557fee75231c5870b675059a47b
miketorii <miketorii@gmail.com>
2024-08-10 23:32:02 +0900

    add object

--- others/value.ts
+++ others/value.ts
@@ -22,3 +22,18 @@ const user : { name: string; age: number } = {
 
 console.log(user.name);
 console.log(user.age);
+
+const product = {
+    name: "Water",
+    price: 100,
+    getTaxIncludedPrice: function () {
+        let val = Math.floor(this.price * 1.1);
+        return val;
+    },
+    expireddate: new Date("2024-08-08"),
+};
+
+console.log(product.name);
+console.log(product.price);
+console.log(product.expireddate);
+console.log(product.getTaxIncludedPrice());
