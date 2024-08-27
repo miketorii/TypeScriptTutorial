9faa55525cdabe3f5b03046f3725cd4ae11eeb0d
miketorii <miketorii@gmail.com>
2024-08-27 22:55:34 +0900

    update

--- others/sentences.ts
+++ others/sentences.ts
@@ -4,10 +4,17 @@ for (const n of numbers) {
     console.log(n);
 }
 
+/*
 const words = ['This', 'is', 'it'];
 
 for (const [idx, w] of words.entries()){
     console.log(idx, w);
+}
+*/
+
+const foo = { a: 1, b: 2};
+for (const prop in foo){
+    console.log(prop, foo[prop]);
 }
 
 
