a2bfba43e3d3efd2b7208afb9cfc03afdb352192
miketorii <miketorii@gmail.com>
2024-08-27 22:46:10 +0900

    add for

---
+++ others/sentences.ts
@@ -0,0 +1,11 @@
+const numbers = [1,2,3];
+
+for (const n of numbers) {
+    console.log(n);
+}
+
+const words = ['This', 'is', 'it'];
+
+for (const [idx, w] of words.entries()){
+    console.log(idx, w);
+}
