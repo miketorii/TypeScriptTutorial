5cfdc28a4290e7299e4a3fff429d94395f444d44
miketorii <miketorii@gmail.com>
2024-09-02 22:21:19 +0900

    add extend

--- classes/person.ts
+++ classes/person.ts
@@ -4,9 +4,9 @@ class Person {
       private _lastName: string;
 
       constructor(ssn: string, firstName: string, lastName: string){
-      		       this.ssn = ssn;
-		       this._firstName = firstName;
-		       this._lastName = lastName;
+      	this.ssn = ssn;
+		this._firstName = firstName;
+		this._lastName = lastName;
       }
 
       getFullName(): string {
@@ -23,6 +23,23 @@ class Person {
 
 }
 
+class Person2 extends Person {
+      private _age : number;
+
+      constructor(ssn: string, firstName: string, lastName: string, age: number){
+            super(ssn, firstName, lastName);
+            this._age = age;
+      }
+
+      get age() {
+            return this._age;
+      }
+
+      set age(argage: number){
+            this._age = argage;
+      }
+}
+
 let message: string = 'Hello, World!';
 console.log(message);
 
@@ -33,3 +50,7 @@ console.log('-------get set---------');
 console.log( person.firstName );
 person.firstName = "John";
 console.log( person.firstName );
+
+console.log('-------extend Person---------');
+let person2 = new Person2('987654321', 'Mike2', 'Torii2', 100);
+console.log( person2.age );
