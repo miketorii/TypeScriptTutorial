a317cd3195d9002641e04bd8aa6b53886fc114b4
miketorii <miketorii@gmail.com>
2024-09-05 14:05:11 +0000

    update json2

--- json/handlejson.ts
+++ json/handlejson.ts
@@ -5,7 +5,7 @@ console.log(device.model);
 console.log(device.type);
 console.log(device.status);
 
-device.model = "iR-ADV C3200";
+device.model = "iR-ADV C3300";
 device.status = 30001;
 console.log(device.model);
 console.log(device.status);
